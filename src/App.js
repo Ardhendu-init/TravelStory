2c54fa96e65159f013e212c2a768589e2a6d1655
Ardhendu Pramanik <ardhendujj002@gmail.com>
2021-08-25 22:19:57 +0530

    changes in logo image in navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import { getPosts } from "./actions/posts";
 import useStyles from "./styles";
-import memories from "./images/travel.png";
+import memories from "./images/globe.png";
 
 const App = () => {
   const [currentId, setCurrentId] = useState(0);
