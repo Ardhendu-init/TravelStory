b7311c96556873255f7f788e766bed82df56b1ce
Ardhendu Pramanik <ardhendujj002@gmail.com>
2021-08-30 21:46:57 +0530

    changes in navabar component decoded token

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link, useHistory, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import useStyles from "./style";
 import globe from "../../images/globe.png";
-import decode from "jwt-decode";
+// import decode from "jwt-decode";
 
 const Navbar = () => {
   const classes = useStyles();
@@ -17,11 +17,11 @@ const Navbar = () => {
     history.push("/");
   };
   useEffect(() => {
-    const token = user?.token;
-    if (token) {
-      const decodeToken = decode(token);
-      if (decodeToken.exp * 1800000 < new Date().getTime()) logout();
-    }
+    // const token = user?.token;
+    // if (token) {
+    //   const decodeToken = decode(token);
+    //   if (decodeToken.exp * 1800000 < new Date().getTime()) logout();
+    // }
     setUser(JSON.parse(localStorage.getItem("profile")));
   }, [location]);
 
